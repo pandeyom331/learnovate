992dc39b8fa43b4b32a700b2abf5aa1aeaf10d2b
Om pandey <54907469+pandeyom331@users.noreply.github.com>
2022-03-11 16:14:20 +0530

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,28 @@
+
+$('#Button1').click(function () {
+    $('#Modal1').modal('show');
+});
+$('#Button2').click(function () {
+    $('#Modal2').modal('show');
+});
+$('#Button3').click(function () {
+    $('#Modal3').modal('show');
+});
+$('#Button4').click(function () {
+    $('#Modal4').modal('show');
+});
+$('#Button5').click(function () {
+    $('#Modal5').modal('show');
+});
+$('#Button6').click(function () {
+    $('#Modal6').modal('show');
+});
+$('#Button7').click(function () {
+    $('#Modal7').modal('show');
+});
+$('#Button8').click(function () {
+    $('#Modal8').modal('show');
+});
+$('#Button9').click(function () {
+    $('#Modal9').modal('show');
+});
